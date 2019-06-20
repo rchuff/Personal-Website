07bd9298554aabe329a7cc1b4d4620ee4a38e4ee
Ryan Huffnagle <Huff@ryans-mbp.attlocal.net>
2019-06-20 17:40:43 -0400

    Update styling to contact and about section

--- index.js
+++ index.js
@@ -5,4 +5,38 @@ $('.down-arrow').click(function(){
   $('.down-arrow').toggleClass('hide');
   $('.about-div').toggleClass('hide show');
   $('nav').css('display','flex');
+  $('.contact-div').removeClass('hide');
 });
+
+var didScroll;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+var lastScrollTop = 0;
+
+$(window).scroll(function(e){
+  didScroll = true;
+});
+
+setInterval(function(){
+  if (didScroll){
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+
+function hasScrolled() {
+  let st = $(this).scrollTop();
+  if (Math.abs(lastScrollTop - st) <= delta) return;
+  if (st > lastScrollTop && st > navbarHeight){
+    $('nav').removeClass('nav-down').addClass('nav-up');
+  } else {
+    if (st + $(window).height() < $(document).height()){
+      $('nav').removeClass('nav-up').addClass('nav-down');
+    }
+  }
+  lastScrollTop = st;
+}
+
+//Once you go past home page, it should open up all of the pages. Contact page
+//should fade in when you reach the bottom of the about page.
